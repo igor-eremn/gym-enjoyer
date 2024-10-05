6f6e3f26d30e17ba93d3b295390123b5ac6fa6ce
Igor Eremin <eremin.igor@icloud.com>
2024-10-05 14:03:53 -0700

    implemented search with util function and search query from input

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Box } from '@mui/material';
 
 import HomeBanner from '../components/HomeBanner';
+import SearchExercises from '../components/SearchExercises';
 
 const Home = () => {
   const [exercises, setExercises] = useState([]);
