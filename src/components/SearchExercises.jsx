e793c9af6a2cced21614bd275a147025fcec1281
Igor Eremin <eremin.igor@icloud.com>
2024-10-05 14:55:13 -0700

    fixed problem with importing env variables

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 
 import { exerciseOptions, fetchData } from '../utils/fetchData';
-import HorizontalScrollbar from './HorizontalScrollbar';
 
 const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
   const [search, setSearch] = useState('');
@@ -50,13 +49,19 @@ const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
           placeholder="Search Exercises"
           type="text"
         />
-        <Button className="search-btn" sx={{ bgcolor: '#FF2625', color: '#fff', textTransform: 'none', width: { lg: '173px', xs: '80px' }, height: '56px', position: 'absolute', right: '0px', fontSize: { lg: '20px', xs: '14px' } }} onClick={handleSearch}>
+        <Button className="search-btn" sx={{ 
+            bgcolor: '#FF2625', color: '#fff', 
+            textTransform: 'none', 
+            width: { lg: '173px', xs: '80px' }, 
+            height: '56px', 
+            position: 'absolute', 
+            right: '0px', 
+            fontSize: { lg: '20px', xs: '14px' } }} 
+            onClick={handleSearch}>
           Search
         </Button>
       </Box>
-      <Box sx={{ position: 'relative', width: '100%', p: '20px' }}>
-        <HorizontalScrollbar data={bodyParts} bodyParts setBodyPart={setBodyPart} bodyPart={bodyPart} />
-      </Box>
+      
     </Stack>
   );
 };
